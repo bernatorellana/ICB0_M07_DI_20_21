5ee6c560bb176578b2cb5a4193838e956a62ccd1
bernat <bernat.orellana@gmail.com>
2020-10-05 21:27:55 +0200

    ListBoxes_Amb_Objectes

---
+++ UF1/20201005_3_demo_listboxes/ListBoxes_Amb_Objectes/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using ListBoxes_Amb_Objectes.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace ListBoxes_Amb_Objectes
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private ObservableCollection<Vehicle> vehicles = new ObservableCollection<Vehicle>();
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Vehicle v1 = new Vehicle("4545FRT", "Seat", "Leon");
+            Vehicle v2 = new Vehicle("6322KKK", "Papamobil", "FI");
+            Vehicle v3 = new Vehicle("8888NNN", "Audi", "A8");
+            //--------------------------------------------
+            vehicles.Add(v1);
+            vehicles.Add(v2);
+            vehicles.Add(v3);
+            //--------------------------------------------
+            lsbVehicles.ItemsSource = vehicles;
+            lsbVehicles.DisplayMemberPath = "NomComplet";
+
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string matricula = txtMatricula.Text;
+            Vehicle nou = new Vehicle(matricula, "Seat", "Leon");
+            vehicles.Add(nou);
+            // 
+            //lsbVehicles.ItemsSource = null;
+            //lsbVehicles.ItemsSource = vehicles;
+        }
+
+        private void txtMatricula_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool matriculaValida =verificaMatriculaHechaPorDani(txtMatricula.Text);
+            btnAlta.IsEnabled = matriculaValida;
+        }
+
+        private bool verificaMatriculaHechaPorDani(string text)
+        {
+            return  Regex.Match(text, "[0-9]{4}[QWRTYPSDFGHJKLZXCVBNM]{3}", RegexOptions.IgnoreCase).Success;
+        }
+    }
+}
---
+++ UF1/20201005_3_demo_listboxes/ListBoxes_Amb_Objectes/Model/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBoxes_Amb_Objectes.Model
+{
+    public class Vehicle
+    {
+        private string marca;
+        private string model;
+        private string matricula;
+
+        public Vehicle(string matricula, string marca, string model)
+        {
+            Matricula = matricula;
+            Marca = marca;
+            Model = model;
+        }
+
+        public string Marca { get => marca.ToUpper(); set => marca = value; }
+        public string Model { get => model; set => model = value; }
+        public string Matricula { get => matricula; set => matricula = value; }
+
+        public string NomComplet
+        {
+            get
+            {
+                return Matricula + ">" + Marca + " " + Model;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Matricula + ">" + Marca + " " + Model;
+        }
+    }
+}
